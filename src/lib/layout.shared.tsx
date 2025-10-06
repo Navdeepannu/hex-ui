db34eab0d238a08803abfd439f4d531ac7bf1a98
Navdeep Singh <84785739+Navdeepannu@users.noreply.github.com>
2025-10-06 09:59:01 -0400

    Error resolved.

---
+++ src/lib/layout.shared.tsx
@@ -0,0 +1,44 @@
+import { IconBrandX } from "@tabler/icons-react";
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import LogoHexagon from "@/components/ui/logo";
+/**
+ * Shared layout configurations
+ *
+ * you can customise layouts individually from:
+ * Home Layout: app/(home)/layout.tsx
+ * Docs Layout: app/docs/layout.tsx
+ */
+export function baseOptions(): BaseLayoutProps {
+  return {
+    githubUrl: "https://github.com/navdeepannu",
+    nav: {
+      transparentMode: "top",
+      title: (
+        <div className="flex gap-1">
+          <LogoHexagon size={30} />
+          <span className="text-xl tracking-tight">Hex UI</span>
+        </div>
+      ),
+      url: "/",
+    },
+    // see https://fumadocs.dev/docs/ui/navigation/links
+    links: [
+      {
+        text: "Documentation",
+        url: "/docs",
+        active: "nested-url",
+      },
+      {
+        text: "Components",
+        url: "/components",
+        active: "nested-url",
+      },
+      {
+        icon: <IconBrandX />,
+        type: "icon",
+        url: "/twitter",
+        text: "X",
+      },
+    ],
+  };
+}
